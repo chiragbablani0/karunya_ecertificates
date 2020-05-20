a1e01bb8188f7a6bda89b3fa9eaf3da7332ae90f
chiragbablani0 <42476557+chiragbablani0@users.noreply.github.com>
2020-05-20 13:09:39 +0530

    added files

---
+++ participants.php
@@ -0,0 +1,10 @@
+<?php
+define('NAMES', [
+    'chirag bablani',
+    'justin mathew',
+    'donald abraham',
+    'joel mathew koshy',
+    'naveen sreevalsan',
+    'pratik ganguly',
+    'sachin vilas nagane',
+]);
