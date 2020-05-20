e7d3ef54cf3f363aebea4674dce7f48db421189f
chiragbablani0 <chiragsonubablani@karunya.edu.in>
2020-05-20 13:30:31 +0530

    changes

--- generate.php
+++ generate.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'data.php';
+require_once 'participants.php';
 
 // Check whether the name submitted is in the array of names.
 if (in_array(strtolower($_POST['name']), NAMES)) {
 	$name = $_POST['name'];
-	require_once 'template.php';
+	require_once 'certificate.php';
 } else {
 	header('Location: index.php?error=name-not-found');
 }
