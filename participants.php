ea1df4ac120300c9c67cb9e57a06dddc7c624ae4
chiragbablani0 <chiragsonubablani@karunya.edu.in>
2020-05-23 10:39:12 +0530

    changes

--- participants.php
+++ participants.php
@@ -1,10 +1,15 @@
 <?php
-define('NAMES', [
-    'chirag bablani',
-    'justin mathew',
-    'donald abraham',
-    'joel mathew koshy',
-    'naveen sreevalsan',
-    'pratik ganguly',
-    'sachin vilas nagane',
-]);
+$connect = mysqli_connect("localhost", "root", "Zerois22", "test");
+if(! $connect ) {
+      die('Could not connect: ' . mysql_error());
+   }
+$sql = mysqli_query($connect,"SELECT * FROM participants");
+ if(! $sql ) {
+      die('Could not get data: ' . mysql_error());
+  }
+   $NAMESS = [];
+while($row = $sql->fetch_assoc()) {
+    array_push($NAMESS,$row["regno"]);
+  }
+
+define('NAMES', $NAMESS);
